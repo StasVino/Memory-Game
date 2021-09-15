7629fa446f29fb5082172987188cd0136d50e9ea
stas vino <78749321+StasVino@users.noreply.github.com>
2021-09-15 23:43:31 +0300

    Add files via upload

---
+++ Memory Game/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+// $G$ SFN-999 (-5) exception when exiting application from login screen
+
+
+
+namespace Memory_Game
+{
+    // $G$ CSS-012 (-5) Access modifiers are missing.
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            GameManager game = new GameManager();
+        }
+    }
+}
+
